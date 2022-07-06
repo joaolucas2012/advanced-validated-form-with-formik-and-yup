d359c5febb822db7411377b4e6cd17dc9bd53a6e
unknown <ribeirojoaolucas68@gmail.com>
2022-07-05 23:47:58 -0300

    create react app with formik

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
